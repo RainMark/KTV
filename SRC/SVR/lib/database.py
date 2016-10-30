b2bf7b751f2a34aedd3cee22a768b6421c2c544f
Bingyu Zhou <rain.by.zhou@gmail.com>
2016-10-30 23:49:55 +0800

    Add hot rank.

--- SRC/SVR/lib/database.py
+++ SRC/SVR/lib/database.py
@@ -10,17 +10,36 @@ class mdb(object):
     def close(self):
         self.database.close()
 
-    def hot100(self):
-        'Return the most hot 100 MV as year rate.'
+    def hot_all(self):
+        'Return the most hot MVs as click rate.'
         query = 'Select * From Song Order By songmonth DESC'
         self.cursor.execute(query)
-        data = self.cursor.fetchmany(200)
-        print(len(data))
+        data = self.cursor.fetchmany(10)
+        # for item in data:
+        #     print(item)
+        return data
+
+    def hot_zh(self):
+        'Return the most hot Chinese MVs as click rate.'
+        query = 'Select * From Song Where songlanguage=\'中文\' Order By songmonth DESC'
+        self.cursor.execute(query)
+        data = self.cursor.fetchmany(10)
         for item in data:
             print(item)
+        return data
+
+    def hot_not_zh(self):
+        'Return the most hot MVs as click rate.'
+        query = 'Select * From Song Where songlanguage!=\'中文\' Order By songmonth DESC'
+        self.cursor.execute(query)
+        data = self.cursor.fetchmany(10)
+        for item in data:
+            print(item)
+        return data
+
 
 if __name__ == '__main__':
     run = mdb('root', 'root', 'ktv_db')
-    run.hot100()
+    run.hot_not_zh()
     run.close()
 

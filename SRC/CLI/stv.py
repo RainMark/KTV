354aadf46d2058e847cb73d4c700d3a788010d2b
Bingyu Zhou <rain.by.zhou@gmail.com>
2016-11-22 10:18:02 +0800

    Start Client Code

--- SRC/CLI/stv.py
+++ SRC/CLI/stv.py
@@ -1,7 +1,19 @@
 #!/usr/python3
 
-from util.stv_request import stv_request_class as stv_req
-from util.stv_ui import stv_ui_class as stv_app
+
+import os
+import sys
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk
+
+sys.path.append(os.path.join(os.getcwd(), 'util'))
+from stv_request import stv_request_class as stv_req
+
+class stv_signal_handler(object):
+    def stv_exit(self, *args):
+        Gtk.main_quit(*args)
+
 
 class stv_class(object):
     def __init__(self):
@@ -10,11 +22,16 @@ class stv_class(object):
         pass
 
     def UI_bind(self):
-        self.app = stv_app()
+        self.builder = Gtk.Builder()
+        self.builder.add_from_file("glade/main.xml")
+        self.window = self.builder.get_object("window")
+        self.builder.connect_signals(stv_signal_handler())
+        self.window.show_all()
 
     def SVR_bind(self):
         self.req = stv_req()
 
 
 if __name__ == '__main__':
-    cli = stv_class()
+    app = stv_class()
+    Gtk.main()
